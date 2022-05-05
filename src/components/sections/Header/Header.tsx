7ebb28d45bb5e1de60e3c05f6737df5c375912a9
Michael <michaelfarquhar4@gmail.com>
2022-05-04 20:52:04 -0400

    Add basic layout

---
+++ src/components/sections/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.title}>MovieBase</div>
+        </div>
+    );
+};
+
+export default Header;
