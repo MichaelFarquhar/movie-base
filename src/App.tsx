996ec67b66bcbc636505fc0a315311930f91cac5
Michael <michaelfarquhar4@gmail.com>
2022-05-07 14:56:19 -0400

    Change theme and header background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Container from './components/layout/Container';
 import Header from './components/sections/Header';
 
 function App() {
     return (
         <div className="app">
-            <Container>
-                <Header />
-            </Container>
+            <Header />
+            <div>
+                {Array(200)
+                    .fill(1)
+                    .map((_, i) => (
+                        <p key={i}>{i}</p>
+                    ))}
+            </div>
+            <div
+                style={{
+                    position: 'fixed',
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    backgroundColor: 'green',
+                }}
+            >
+                footer
+            </div>
         </div>
     );
 }
--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Container from '../../layout/Container';
 import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <div className={styles.header}>
-            <div className={styles.title}>MovieBase</div>
-            <input className={styles.search} placeholder="Search movies" />
+        <div className={styles.headerContainer}>
+            <Container>
+                <div className={styles.header}>
+                    <div className={styles.title}>MovieBase</div>
+                    <input className={styles.search} placeholder="Search movies" />
+                </div>
+            </Container>
         </div>
     );
 };
