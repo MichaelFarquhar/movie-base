550c7765bf76f74afc3ed9d88f99d652d1f6735d
Michael <michaelfarquhar4@gmail.com>
2022-05-09 20:05:28 -0400

    Add informtion to footer

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import Container from '../Container';
 import styles from './Footer.module.css';
+import { FaGithub } from 'react-icons/fa';
+import { SiThemoviedatabase } from 'react-icons/si';
 
 const Footer = () => {
     return (
         <div className={styles.footerContainer}>
             <Container>
-                <div className={styles.footer}>Footer</div>
+                <div className={styles.footer}>
+                    <span>By: Michael Farquhar</span>
+                    <div className={styles.footer__icons}>
+                        <a
+                            href="https://github.com/MichaelFarquhar/movie-base"
+                            target="blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                        </a>
+                        <a
+                            href="https://developers.themoviedb.org/3"
+                            target="blank"
+                            rel="noopener noreferrer"
+                        >
+                            <SiThemoviedatabase />
+                        </a>
+                    </div>
+                </div>
             </Container>
         </div>
     );
